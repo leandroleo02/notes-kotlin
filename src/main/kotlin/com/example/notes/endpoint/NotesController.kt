cfc81a09a1d5b54848a2ca58bf9bfd2d8cefbb25
Leandro Rodrigues <leandroleo02@gmail.com>
2020-09-19 00:58:49 -0300

    Change Note controller to use notes repository

--- src/main/kotlin/com/example/notes/endpoint/NotesController.kt
+++ src/main/kotlin/com/example/notes/endpoint/NotesController.kt
@@ -1,17 +1,19 @@
 package com.example.notes.endpoint
 
+import com.example.notes.domain.Note
+import com.example.notes.domain.NotesRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController()
+@RestController
 @RequestMapping("notes")
-class NotesController {
+class NotesController(private val notesRepository: NotesRepository) {
 
     @GetMapping
     fun retrieveNotes(): List<NotesResponse> {
-        return listOf(NotesResponse("1", "First note in Kotlin"))
+        return notesRepository.retrieveAll().map { note -> NotesResponse(note) }
     }
 
     @GetMapping("{id}")
@@ -20,4 +22,7 @@ class NotesController {
     }
 }
 
-class NotesResponse(val id: String, val text: String)
+class NotesResponse(val id: String, val text: String) {
+
+    constructor(note: Note): this(note.id, note.text)
+}
