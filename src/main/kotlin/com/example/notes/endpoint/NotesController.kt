4276c8bf558fc234b7509bfe8d46ce0ac40e7861
Leandro Rodrigues <leandroleo02@gmail.com>
2020-09-19 01:24:27 -0300

    Find notes by id

--- src/main/kotlin/com/example/notes/endpoint/NotesController.kt
+++ src/main/kotlin/com/example/notes/endpoint/NotesController.kt
@@ -2,6 +2,8 @@ package com.example.notes.endpoint
 
 import com.example.notes.domain.Note
 import com.example.notes.domain.NotesRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,8 +19,9 @@ class NotesController(private val notesRepository: NotesRepository) {
     }
 
     @GetMapping("{id}")
-    fun getNoteById(@PathVariable("id") id: String): NotesResponse {
-        return NotesResponse("1", "First note in Kotlin")
+    fun getNoteById(@PathVariable("id") id: String): ResponseEntity<NotesResponse> {
+        val note = notesRepository.findById(id) ?: return ResponseEntity(HttpStatus.NOT_FOUND)
+        return ResponseEntity(NotesResponse(note), HttpStatus.OK)
     }
 }
 
