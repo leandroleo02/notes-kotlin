094161cf529fe5a96955ade6981179e76a9af400
Leandro Rodrigues <leandroleo02@gmail.com>
2020-09-19 12:47:44 -0300

    Respository reading file from stream

--- src/main/kotlin/com/example/notes/infrastructure/CsvNotesRepository.kt
+++ src/main/kotlin/com/example/notes/infrastructure/CsvNotesRepository.kt
@@ -3,19 +3,18 @@ package com.example.notes.infrastructure
 import com.example.notes.domain.Note
 import com.example.notes.domain.NotesRepository
 import org.springframework.stereotype.Repository
+import java.io.InputStream
 
 @Repository
 class CsvNotesRepository(private val csvReader: CsvReader): NotesRepository {
 
-    private val filePath = filePath()
-
     override fun retrieveAll(): List<Note> {
-        return csvReader.readFile(filePath!!)
+        return csvReader.readFile(file()!!)
                 .map(::toNotes)
     }
 
     override fun findById(id: String): Note? {
-        return csvReader.readFile(filePath!!)
+        return csvReader.readFile(file()!!)
                 .map(::toNotes)
                 .find { note -> note.id == id }
     }
@@ -24,8 +23,8 @@ class CsvNotesRepository(private val csvReader: CsvReader): NotesRepository {
         return Note(line[0], line[1])
     }
 
-    private fun filePath(): String? {
+    private fun file(): InputStream? {
         return this::class.java.classLoader
-                .getResource("notes/notes.csv")?.path
+                .getResourceAsStream("notes/notes.csv")
     }
 }
