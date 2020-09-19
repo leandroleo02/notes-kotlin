8a02cde0621488763fba2da4612aeb442ed94dc0
Leandro Rodrigues <leandroleo02@gmail.com>
2020-09-19 12:45:10 -0300

    Create csv reader from stream

--- src/main/kotlin/com/example/notes/infrastructure/CsvReader.kt
+++ src/main/kotlin/com/example/notes/infrastructure/CsvReader.kt
@@ -4,13 +4,25 @@ import com.opencsv.CSVReader
 import org.springframework.stereotype.Component
 import java.io.BufferedReader
 import java.io.FileReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.Reader
 
 @Component
 class CsvReader {
 
     fun readFile(filePath: String): List<Array<String>> {
         val fileReader = BufferedReader(FileReader(filePath))
-        val csvReader = CSVReader(fileReader)
+        return readFile(fileReader)
+    }
+
+    fun readFile(fileStream: InputStream): List<Array<String>> {
+        val fileReader = BufferedReader(InputStreamReader(fileStream))
+        return readFile(fileReader)
+    }
+
+    private fun readFile(reader: Reader): List<Array<String>> {
+        val csvReader = CSVReader(reader)
         csvReader.skip(1)
         return csvReader.readAll()
     }
--- src/test/kotlin/com/example/notes/infrastructure/CsvReaderTest.kt
+++ src/test/kotlin/com/example/notes/infrastructure/CsvReaderTest.kt
@@ -16,6 +16,13 @@ class CsvReaderTest {
     fun readDataFromFile() {
         val fileUrl = this::class.java.classLoader.getResource("notes/notes.csv")
         val csvReader = CsvReader()
-        assertThat(csvReader.readFile(fileUrl.path)).hasSize(5)
+        assertThat(csvReader.readFile(fileUrl!!.path)).hasSize(5)
+    }
+
+    @Test
+    fun readDataFromStream() {
+        val fileStream = this::class.java.classLoader.getResourceAsStream("notes/notes.csv")
+        val csvReader = CsvReader()
+        assertThat(csvReader.readFile(fileStream!!)).hasSize(5)
     }
 }
